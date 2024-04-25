a37eac7ee0120d1af982bc1eee20ee54b8a69c2b
isabellamo <142440827+isabellamo@users.noreply.github.com>
2024-04-25 12:07:24 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -10,9 +10,11 @@ public class LightBoard
    */
   public LightBoard(int numRows, int numCols){
     /* to be implemented in part (a) */
+    lights = new boolean[numRows][numCols];
+    
     for(int r = 0; r < numRows; r++){
       for(int c = 0; c < numCols; c++){
-        if(Math.random() <= 0.4){
+        if(Math.random() < 0.4){
           lights[r][c] = true;
         } else {
           lights[r][c] = false;
